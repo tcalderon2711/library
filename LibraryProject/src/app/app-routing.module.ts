562604d5755dde711c26f052a20baa6d1574ea5c
tcalderon2711 <121997280+tcalderon2711@users.noreply.github.com>
2024-08-28 12:03:15 -0500

    Created components
    Home, books,events,locations,about us and header components

--- LibraryProject/src/app/app-routing.module.ts
+++ LibraryProject/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { BooksComponent } from './books/books.component';
+import { EventsComponent } from './events/events.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { LocationsComponent } from './locations/locations.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home',component:HomeComponent},
+  {path:'books', component:BooksComponent},
+  {path:'events',component:EventsComponent},
+  {path:'locations',component:LocationsComponent},
+  {path:'about-us',component:AboutUsComponent},
+  {path:'',redirectTo:'home',pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- LibraryProject/src/app/app.module.ts
+++ LibraryProject/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { BooksComponent } from './books/books.component';
+import { EventsComponent } from './events/events.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { LocationsComponent } from './locations/locations.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    BooksComponent,
+    EventsComponent,
+    AboutUsComponent,
+    LocationsComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
