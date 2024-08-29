0a5aab66ad23b2569b860ca51959ad0349f91dbc
Emerson Clay <136268542+c1ayer@users.noreply.github.com>
2024-08-28 19:17:15 -0500

    Add files via upload

---
+++ library-main/library-main/LibraryProject/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { BooksComponent } from './books/books.component';
+import { EventsComponent } from './events/events.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { LocationsComponent } from './locations/locations.component';
+import { HeaderComponent } from './header/header.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    BooksComponent,
+    EventsComponent,
+    AboutUsComponent,
+    LocationsComponent,
+    HeaderComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule, 
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
